94bc9aba11466a1b7ee7f6defa5a6692116a1dfa
Ahmad Hossain <ahmad.awaab@gmail.com>
2022-04-19 16:55:22 -0400

    - Setup TimePickerDialog with tv - Added border to reminder-time tv

--- app/src/main/java/com/example/petapp/fragments/TaskComposeFragment.kt
+++ app/src/main/java/com/example/petapp/fragments/TaskComposeFragment.kt
@@ -1,15 +1,20 @@
 package com.example.petapp.fragments
 
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
-import android.widget.TimePicker
 import com.example.petapp.R
+import com.example.petapp.models.Task
+
+private const val TAG = "TaskComposeFragment"
 
 class TaskComposeFragment : Fragment() {
 
@@ -18,7 +23,43 @@ class TaskComposeFragment : Fragment() {
     lateinit var etDescription: EditText
     lateinit var tvReminderTime: TextView
     lateinit var spRepeat: Spinner
-    lateinit var timePicker: TimePicker
+    lateinit var btSave: Button
+    val timePickerDialogListener: TimePickerDialog.OnTimeSetListener =
+        TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> // logic to properly handle
+            // the picked timings by user
+            val formattedTime: String = when {
+                hourOfDay == 0 -> {
+                    if (minute < 10) {
+                        "${hourOfDay + 12}:0${minute} am"
+                    } else {
+                        "${hourOfDay + 12}:${minute} am"
+                    }
+                }
+                hourOfDay > 12 -> {
+                    if (minute < 10) {
+                        "${hourOfDay - 12}:0${minute} pm"
+                    } else {
+                        "${hourOfDay - 12}:${minute} pm"
+                    }
+                }
+                hourOfDay == 12 -> {
+                    if (minute < 10) {
+                        "${hourOfDay}:0${minute} pm"
+                    } else {
+                        "${hourOfDay}:${minute} pm"
+                    }
+                }
+                else -> {
+                    if (minute < 10) {
+                        "${hourOfDay}:${minute} am"
+                    } else {
+                        "${hourOfDay}:${minute} am"
+                    }
+                }
+            }
+
+            tvReminderTime.text = formattedTime
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,7 +77,44 @@ class TaskComposeFragment : Fragment() {
         etDescription = view.findViewById(R.id.et_description)
         tvReminderTime = view.findViewById(R.id.tv_reminder_time)
         spRepeat = view.findViewById(R.id.sp_repeat)
-        timePicker = view.findViewById(R.id.timepicker)
+        btSave = view.findViewById(R.id.bt_save)
+
+        tvReminderTime.setOnClickListener {
+            Log.d(TAG, "Reminder time clicked")
+
+            val timePicker: TimePickerDialog = TimePickerDialog(
+                // pass the Context
+                requireContext(),
+                // listener to perform task when time is picked
+                timePickerDialogListener,
+                // default hour when the time picker dialog is opened
+                12,
+                // default minute when the time picker dialog is opened
+                10,
+                // 24 hours time picker is false (varies according to the region)
+                false
+            )
+
+            // then after building the timepicker
+            // dialog show the dialog to user
+            timePicker.show()
+        }
+
+        //send the task to Parse
+        btSave.setOnClickListener {
+            val title = etTitle.text.toString()
+            val description = etDescription.text.toString()
+            val reminderTime = tvReminderTime.text.toString()
+            //todo 1. Validate input
+
+            //todo 2. Create Object
+            val task = Task().apply {
+                setTitle(title)
+                setDescription(description)
+            }
+
+            //todo 3. Send object to Parse
+        }
 
     }
 
