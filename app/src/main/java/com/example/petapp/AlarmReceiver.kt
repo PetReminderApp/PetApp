398430e8cf63601288cdf427a76c1727b74813d1
Ahmad Hossain <ahmad.awaab@gmail.com>
2022-04-27 00:01:28 -0400

    notif is given for each incomplete Task

--- app/src/main/java/com/example/petapp/AlarmReceiver.kt
+++ app/src/main/java/com/example/petapp/AlarmReceiver.kt
@@ -7,21 +7,46 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.os.SystemClock
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-import java.lang.Exception
+import com.example.petapp.util.ParseUtil
 
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         try {
-            showNotification(context, "Title", "description")
+            showTaskNotifications(context)
         } catch (e: Exception) {
             Log.e(TAG, "onReceive: ${e.printStackTrace()}")
         }
     }
 
-    private fun showNotification(context: Context, title: String, description: String) {
+    private fun showTaskNotifications(context: Context) {
+        //unique ID for each notification
+        val notificationId = SystemClock.uptimeMillis().toInt()
+
+        ParseUtil.queryPets { pet ->
+            ParseUtil.queryPetTasks(
+                pet,
+                callback = { task ->
+                    if (task.getCompleted() == false) {
+                        //create notification for task
+                        task.getTitle()?.let { showNotification(context, "Incomplete Task", it, notificationId) }
+                        Log.d(TAG,"showTaskNotifications: $notificationId Incomplete Task: ${task.getTitle()}")
+                    }
+                },
+                {}
+            )
+        }
+    }
+
+    private fun showNotification(
+        context: Context,
+        title: String,
+        description: String,
+        notificationId: Int
+    ) {
         val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         val i = Intent(context, MainActivity::class.java)
@@ -29,7 +54,8 @@ class AlarmReceiver : BroadcastReceiver() {
         val pendingIntent = PendingIntent.getActivity(context, 0, i, 0)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+            val channel =
+                NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
             manager.createNotificationChannel(channel)
         }
 
@@ -44,7 +70,7 @@ class AlarmReceiver : BroadcastReceiver() {
 
         val notificationManager = NotificationManagerCompat.from(context)
 
-        notificationManager.notify(1, notification.build())
+        notificationManager.notify(notificationId, notification.build())
     }
 
     companion object {
